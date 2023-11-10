0a854bd5ab223b6fa853f3d4f62f385f30538271
lixu26 <lixu26@baidu.com>
2023-11-10 12:20:56 +0800

    23.11.10 mac

--- Codeforces/1829(div4)/B.cpp
+++ Codeforces/1829(div4)/B.cpp
@@ -1,13 +1,16 @@
-#ifdef LOCAL
-#include "F:/coder/acm/dbg.hpp"
+#ifdef FILEIO
+#ifdef __APPLE__
+#define INPUT_FILE "/Users/lixu26/work/ACM/input.txt"
+#define OUTPUT_FILE "/Users/lixu26/work/ACM/output.txt"
+#define ERROR_FILE "/Users/lixu26/work/ACM/error.txt"
+#else
 #define INPUT_FILE "F:/coder/acm/input.txt"
 #define OUTPUT_FILE "F:/coder/acm/output.txt"
 #define ERROR_FILE "F:/coder/acm/error.txt"
 #endif
+#endif
+
 #include <bits/stdc++.h>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds; //required
 using namespace std;
 
 #define PB push_back
@@ -24,6 +27,7 @@ using namespace std;
 #define bitcnt_headzero(x) (__builtin_clz(x))   // 返回x的二进制开头0的数量
 #define SZ(x) (int)(x.size())
 #define log2(x) log(x) / log(2)
+#define lcm(a, b) (LL)(a) * (b) / __gcd(a, b)
 
 using VI = std::vector<int>;
 using VVI = std::vector<VI>;
@@ -34,16 +38,7 @@ using VVB = std::vector<std::vector<bool>>;
 using PII = std::pair<int, int>;
 using PLL = std::pair<LL, LL>;
 
-// 支持下标访问的ordered_set/ordered_multiset
-// 使用:
-//      ordered_set<int> s; 或 ordered_multiset<int> s;
-//      s.find_by_order(下标); // 返回s[下标]的迭代器
-//      s.order_of_key(x); // 返回s中严格<x的元素个数
-template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template<typename T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 template<typename T1, typename T2> T1 MOD(T1 a, T2 m){return (T1)(a%m+m)%m;} // 求 a%m
-LL gcd(LL a,LL b){return __gcd(a,b);} // a和b的最大公约数
-LL lcm(LL a,LL b){return a/__gcd(a, b)*b;} // a和b的最小公倍数
 LL quick_power(LL x, LL y, LL mod){LL res=1,cur=x;while(y){if(y&1)res=res*cur%mod;cur=cur*cur%mod;y>>=1;}return (res%mod+mod)%mod;}
 inline int combination(int n,int k){int sum=0;if(n==k||k==0){return 1;}else{return combination(n-1,k)+ combination(n-1,k-1);}}
 inline int read(){int x=0,f=1;char ch=getchar();while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}while(ch>='0' && ch<='9')x=x*10+ch-'0',ch=getchar();return x*f;}
@@ -61,33 +56,37 @@ const int inf = 0x3f3f3f3f, INF = 0x7f7f7f7f;
 
 */
 
-
 void solve() {
-    int n, d;
-    cin >> n >> d;
-    string s;
-    cin >> s;
-    bool flg = false;
-    rep(i, 0, n) {
-        if (!flg && d > s[i] - '0') {
-            cout << d;
-            flg = true;
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int ans = 0, cur = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        if (!a[i]) {
+            ++cur;
+        } else {
+            cur = 0;
         }
-        cout << s[i];
+        ans = max(ans, cur);
     }
-    if (!flg) cout << d;
-    cout << "\n";
+    cout << ans << endl;
 }
 
+
+
+
 int main() {
-#ifdef LOCAL
+#ifdef FILEIO
     freopen(INPUT_FILE, "r", stdin); freopen(OUTPUT_FILE, "w", stdout); freopen(ERROR_FILE, "w", stderr);
 #endif
     ios::sync_with_stdio(false); cin.tie(nullptr);
 
     int t = 1;
     cin >> t;
-    while (t--) solve();
+    while (t--) {
+        solve();
+    }
 
     return 0;
 }
---
+++ build/CMakeFiles/A.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "A"
+  "A.pdb"
+  "CMakeFiles/A.dir/Codeforces/1797(div2)/A.cpp.o"
+  "CMakeFiles/A.dir/Codeforces/1797(div2)/A.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
