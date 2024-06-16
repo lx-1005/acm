8eb01555d249d4a32640453b5cb2447d0de7d04e
lixu26 <1216069415@qq.com>
2024-06-16 15:15:05 +0800

    cf 1352

--- cf/contest/1352/1352B.cpp
+++ cf/contest/1352/1352B.cpp
@@ -1,6 +1,6 @@
-/*
- * Author: $%U%$
- * Time:   $%Y%$-$%M%$-$%D%$ $%h%$:$%m%$:$%s%$
+/** 
+ *     author:  JiuR
+ *     created: 2024-06-16 00.36.11
 **/
 #include <bits/stdc++.h>
 using namespace std;
@@ -16,7 +16,6 @@ using i128 = __int128;
 #define bitcnt_tailzero(x) __builtin_ctz(x) // 返回x的二进制末尾0的数量，等价于x的最低位1是第几位
 #define bitcnt_headzero(x) (__builtin_clz(x)) // 返回x的二进制开头0的数量
 #define sz(x) (int)(x.size())
-
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 template <typename T1, typename T2> T1 quick_mod(T1 a, T2 m) { return (T1)(a % m + m) % m; } // 求 a%m 
 ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }     // a和b的最小公倍数
@@ -30,22 +29,50 @@ const int inf = 0x3f3f3f3f;
 
 /*
 
+假设存在偶数方案，使得n=a1+a2+...+ak，那么将a[1~k-1]变成2，多出来的部分放到ak上，一定也是一种偶数方案
+奇数方案同理，a[1~k-1]变成1，最后一个n-(k-1)也是奇数
 
 
 */
 
 void solve() {
+    int n, k;
+    cin >> n >> k;
 
+    int n1 = n-(k-1)*2;
+    if (n1 > 0 && n1%2==0) {
+        cout << "YES" << '\n';
+        for (int i = 0; i < k - 1; i++) cout << 2 << ' ';
+        cout << n-(k-1)*2 << '\n';
+        return;
+    } 
+
+    int n2 = n-(k-1);
+    if (n2 > 0 && n2%2) {
+        cout << "YES" << '\n';
+       for (int i = 0; i < k - 1; i++) cout << 1 << ' ';
+        cout << n-(k-1) << '\n'; 
+        return;
+    }
+
+    cout << "NO" << '\n';
 }
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    auto start_time = clock();
+
     int T = 1;
-    // cin >> T;
+    cin >> T;
     while (T--) {
         solve();
     }
 
+    auto end_time = clock();
+#ifdef LOCAL
+    std::cerr << "Running time is: " << static_cast<double>(end_time - start_time) / CLOCKS_PER_SEC * 1000 << "ms"
+              << "\n";  // 输出运行时间
+#endif
+
     return 0;
 }
-
